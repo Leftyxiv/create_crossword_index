f509e53a65868a58b9d4b44d5dd3424c00870a19
Leftyxiv <manny@aledoux.net>
2021-02-26 13:12:45 -0500

    finished maybe

--- crossword.py
+++ crossword.py
@@ -9,6 +9,9 @@ def create_crossword_indices():
       if sschar.upper() == fschar.upper():
         tuple_list.append((i,j))
 
+  # sorted_list = sorted(tuple_list, key=lambda tup: (tup[0] - 1) * (tup[1] - 1))
+  # print(sorted_list)
+  print(tuple_list)
   if not tuple_list:
     return None
   else:
